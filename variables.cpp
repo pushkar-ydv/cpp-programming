04de47f145f6da0bbad2704733b557ca13451865
pushkar-ydv <jpyadav73515@gmail.com>
2026-01-26 23:22:58 +0530

    Add files via upload

---
+++ variables.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+int main(){
+
+    //integer (whole number)
+
+    int age = 29;
+    int year = 2026;
+    int days = 7;
+
+    // double (number including decimal)
+    double price = 30.66;
+    double gpa = 3.5;
+    double temperature =32.5;
+
+    //single character 
+    char grade = 'A';
+    char initial = 'P';
+    char currency = '$';
+
+    //boolean(true and false)
+    bool student = false;
+    bool power = true;
+
+    //string(object that represent a sequece of text)
+    std::string name = "pushkar";
+    std::string day = "friday";
+    std::string food = "pizza";
+    std:: string address = "23jfdj";
+
+    std::cout << "hello " << name << '\n';
+    std::cout << "you are " << age << "year old" << '\n';
+
+
+    return 0;
+}
