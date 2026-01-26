e6fd97731f05919e03f29ed4d8ae3015055088dc
pushkar-ydv <jpyadav73515@gmail.com>
2026-01-26 23:30:57 +0530

    Add files via upload

---
+++ constant.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+
+    const double PI = 3.14;
+    PI =244;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    std::cout << circumference;
+
+    return 0;
+}
