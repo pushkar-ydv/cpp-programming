aea6e4afb81a4804447cff77ce44d9a729d2aa28
pushkar-ydv <jpyadav73515@gmail.com>
2026-01-27 19:00:37 +0530

    Add files via upload

---
+++ arithmaticOperator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int main(){
+
+    int students = 20;
+
+    //students = students + 1;
+    //students+=1;
+    //students++;
+    
+    //students = students - 1;
+    //students-=1;
+    //students--;
+
+    //students = students * 2;
+    //students*=2;
+    
+    //students = students / 2;
+    //students/=2;
+
+    int remainder = students % 2;
+
+
+    std::cout << remainder;
+
+    return 0;
+}
